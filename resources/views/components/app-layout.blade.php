eb6a570be6cd142dc2580a8fa7f32e802434e7d9
Aczeko <amina.cajlakovic@stud.edubs.ch>
2024-06-28 17:47:05 +0200

    final update

--- resources/views/components/app-layout.blade.php
+++ resources/views/components/app-layout.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Laravel Static Blog</title>
 
         @vite('resources/css/app.css')
 
@@ -13,8 +13,9 @@
         <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
     </head>
     <body class="antialiased">
-        <div class="max-w-2xl mx-auto py-16 px-4 md:px-0">
+        <div class="max-w-2xl mx-auto py-16 px-4">
             <a href="{{ route('posts.index') }}" class="inline-block text-2xl font-extrabold hover:text-blue-500 transition-colors duration-100">{{ config('app.name') }}</a>
+
             <main class="mt-16">
                 {{ $slot }}
             </main>
